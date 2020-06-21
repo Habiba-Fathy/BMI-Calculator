d0465286a9f3837ba527c3a596084eff23dbcc79
Habiba-Fathy <sci.habibamohamed2018@alexu.edu.eg>
2020-06-21 22:22:38 +0200

    BMI

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({@required this.buttonText, @required this.onPressButton});
+
+  final String buttonText;
+  final Function onPressButton;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressButton,
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonText,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColour,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 10.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
